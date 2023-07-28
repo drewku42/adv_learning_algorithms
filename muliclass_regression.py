9d2e44ec1978562419baf3e13743ca61c2fffcae
Drew Meyer <drewmeyer28@gmail.com>
2023-07-28 10:00:41 -0500

    Update muliclass_regression.py

--- muliclass_regression.py
+++ muliclass_regression.py
@@ -9,16 +9,20 @@ from tensorflow.keras import Sequential # import sequential model
 from tensorflow.keras.layers import Dense # import forward prop layer architecture
 from tensorflow.keras.losses import SparseCategoricalCrossentropy # import crossentropy loss function
 
+# define model architecture
 model = Sequential([
   Dense(units=25, activation='relu'), # define first layer with 25 neurons and relu activation function
   Dense(units=15, activation='relu'), # define second layer with 15 neurons and relu activation function
   Dense(units=10, activation='linear') # define third and final layer
 ]) 
 
-model.compile(loss = SparseCategoricalCrossentropy(from_logits=True) ) # specify loss and cost function
+# specify loss and cost function
+model.compile(loss = SparseCategoricalCrossentropy(from_logits=True) )
 # from_logits=True improves rounding errors for numnerical accuracy
 
-model.fit(X, Y, epochs=100) # train on data to minimize cost function
+# train on data to minimize cost function
+model.fit(X, Y, epochs=100)
 
+# predict class for given digit
 logits = model(X)
-f_x = tf.nn.softmax(logits) # predict class for given digit
+f_x = tf.nn.softmax(logits)
